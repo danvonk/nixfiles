55fd430b71b1803611c148d0038b561202cfb6a7
Dan Vonk <dan@danvonk.com>
2023-11-25 21:42:26 +0100

    Formatting

--- home/home.nix
+++ home/home.nix
@@ -1,96 +1,97 @@
-{ inputs, config, pkgs, ... }:
-{
-  imports = [
-    inputs.nix-doom-emacs.hmModule
-    ./git.nix
-  ];
+{ inputs, config, pkgs, lib, ... }: {
+  imports = [ inputs.nix-doom-emacs.hmModule ./git.nix ];
+
+  nixpkgs.config = {
+    allowUnfree = true;
+    allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [ "vscode" ];
+  };
 
   home = {
     username = "dan";
     homeDirectory = "/home/dan";
   };
 
-    /* The home.stateVersion option does not have a default and must be set */
-    home.stateVersion = "23.05";
-    programs.home-manager.enable = true;
-    
-    fonts.fontconfig.enable = true;
+  # The home.stateVersion option does not have a default and must be set
+  home.stateVersion = "23.05";
+  programs.home-manager.enable = true;
 
-     dconf.settings = {
-       "org/gnome/mutter" = {
-         experimental-features = [];
-       };
-     };
+  fonts.fontconfig.enable = true;
 
-    home.packages = with pkgs; [
-      vim
-      git
-      htop
-      firefox
-      thunderbird
-      zsh
-      rofi
-      ripgrep
-      kitty
-      jetbrains-mono
-      noto-fonts
-      xfce.xfce4-whiskermenu-plugin
-      ispell
-      hunspell
-      hunspellDicts.en-gb-large
-      pavucontrol
-      syncthing
-      direnv
-      nix-direnv
-      nixfmt
-      ripgrep
-      libreoffice-qt
-      foliate
-      texlive.combined.scheme-full
-      isabelle
-      gdb
-    ];
+  dconf.settings = { "org/gnome/mutter" = { experimental-features = [ ]; }; };
 
-    home.sessionVariables = {
-      EDITOR = "emacsclient -nw";
-      NIXPKGS_ALLOW_BROKEN = 1;
-    };
+  home.packages = with pkgs; [
+    vim
+    git
+    htop
+    firefox
+    thunderbird
+    zsh
+    rofi
+    ripgrep
+    kitty
+    jetbrains-mono
+    noto-fonts
+    xfce.xfce4-whiskermenu-plugin
+    ispell
+    hunspell
+    hunspellDicts.en-gb-large
+    pavucontrol
+    syncthing
+    direnv
+    nix-direnv
+    nixfmt
+    ripgrep
+    libreoffice-qt
+    foliate
+    texlive.combined.scheme-full
+    isabelle
+    gdb
+    vscode
+    ghc # having ghci in shell is useful
+    agda
+  ];
 
-    home.shellAliases = {
-      nupdate = "cd ~/nixfiles && sudo nixos-rebuild switch --upgrade --flake";
-      hupdate = "cd ~/nixfiles && home-manager switch --flake .#dan@desktop && cd -";
-    };
+  home.sessionVariables = {
+    EDITOR = "emacsclient -nw";
+    NIXPKGS_ALLOW_BROKEN = 1;
+  };
 
-    programs.zsh = {
+  home.shellAliases = {
+    nupdate = "cd ~/nixfiles && sudo nixos-rebuild switch --upgrade --flake";
+    hupdate =
+      "cd ~/nixfiles && home-manager switch --flake .#dan@desktop && cd -";
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    oh-my-zsh = {
       enable = true;
-      enableCompletion = true;
-      oh-my-zsh = {
-        enable = true;
-        plugins = [
-          "git"
-                  ];
-        theme = "robbyrussell";
-      };
+      plugins = [ "git" ];
+      theme = "robbyrussell";
     };
+  };
 
-    programs.doom-emacs = {
-      enable = true;
-      doomPrivateDir = ./doom.d; # Directory containing your config.el, init.el
-                                 # and packages.el files
-    };
+  programs.doom-emacs = {
+    enable = true;
+    doomPrivateDir = ./doom.d; # Directory containing your config.el, init.el
+    # and packages.el files
+  };
 
-    programs.direnv = {
-      enable = true;
-      nix-direnv.enable = true;
-      enableZshIntegration = true;
-      enableBashIntegration = true;
-    };
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+    enableZshIntegration = true;
+    enableBashIntegration = true;
+  };
 
-    services.syncthing = {
-      enable = true;
-      tray.enable = true;
-    };
+  services.syncthing = {
+    enable = true;
+    tray.enable = true;
+  };
 
-    home.file.".config/kitty/kitty.conf".source = config.lib.file.mkOutOfStoreSymlink ./dotfiles/kitty/kitty.conf;
-    home.file.".config/i3/config".source = config.lib.file.mkOutOfStoreSymlink ./dotfiles/i3/config;
+  home.file.".config/kitty/kitty.conf".source =
+    config.lib.file.mkOutOfStoreSymlink ./dotfiles/kitty/kitty.conf;
+  home.file.".config/i3/config".source =
+    config.lib.file.mkOutOfStoreSymlink ./dotfiles/i3/config;
 }
