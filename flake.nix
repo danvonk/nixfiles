60c11635c6075a01ed39b1213e76995a86da7c56
Dan Vonk <dan@danvonk.com>
2023-12-07 10:01:10 +0100

    Formatting

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
-  inputs.home-manager.url = "github:nix-community/home-manager/release-23.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+  inputs.home-manager.url = "github:nix-community/home-manager/release-23.11";
   inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
   inputs.nix-doom-emacs.url = "github:nix-community/nix-doom-emacs";
 
@@ -11,26 +11,23 @@
       desktop = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = { inherit inputs; };
-        modules =
-          [
-            ./machines/desktop/default.nix
-          ];
+        modules = [ ./machines/desktop/default.nix ];
       };
       nixps = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = { inherit inputs; };
-        modules =
-          [
-            ./machines/nixps/default.nix
-          ];
+        modules = [ ./machines/nixps/default.nix ];
       };
 
     };
     #  Home Manager config for dan@desktop
     homeConfigurations = {
       "dan@desktop" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-        extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
+        pkgs =
+          nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = {
+          inherit inputs;
+        }; # Pass flake inputs to our config
         modules = [ ./home/home.nix ];
       };
     };
