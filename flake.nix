d04ad6d1328c977f728560a72e0602383152877b
Dan Vonk <dan@danvonk.com>
2025-03-04 13:26:37 +0100

    Update nix flake

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-  inputs.home-manager.url = "github:nix-community/home-manager/release-24.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  inputs.home-manager.url = "github:nix-community/home-manager/release-24.11";
   inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
   outputs = { self, nixpkgs, home-manager }@inputs: {
